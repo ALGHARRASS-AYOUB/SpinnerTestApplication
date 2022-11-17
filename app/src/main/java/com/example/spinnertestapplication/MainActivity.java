3cc5c0b95ba88122c3d8d1558313a26057ff652e
ALGHARRASS-AYOUB <ALGHARRASS.ayoub@gmail.com>
2022-11-17 17:02:40 +0100

    view almost

--- app/src/main/java/com/example/spinnertestapplication/MainActivity.java
+++ app/src/main/java/com/example/spinnertestapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.spinnertestapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Spinner spinner=findViewById(R.id.spinner_guns);
+//        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,
+//                R.array.guns_list, R.layout.spinner_layout);
+//        adapter.setDropDownViewResource(R.layout.spinner_layout);
+//        spinner.setAdapter(adapter);
     }
 }
